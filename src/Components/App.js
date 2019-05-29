e146a16e25d3ffd843fad955a969e5eede27de08
Juan Duran <jcduran05@gmail.com>
2019-05-28 23:39:05 -0700

    Added basic firebase integration.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from '../firebase.js'; // configs ignored file
 import { useInput } from '../Hooks/InputHook';
 import './App.css';
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -13,6 +14,7 @@ import Analysis from './Analysis/';
 1. Find way to make sure state and formData match
 2. onChange should not change int to strings
 3. Add routing to break form into sections and final analysis page
+4  Storage through firebase to retrieve old reports
 */
 
 class App extends Component {
@@ -20,47 +22,51 @@ class App extends Component {
     super(props);
       // const { value:firstName, bind:bindFirstName, reset:resetFirstName } = useInput('');
       // const { value:lastName, bind:bindLastName, reset:resetLastName } = useInput('');
-      this.state = {
-        // Property Info
-        reportTitle: '',
-        propertyAddress: '',
-        city: '',
-        state: '',
-        zip: '',
-        
-        // Purchase Info
-        purchasePrice: 0,
-        repairCost: 0,
-        annualPropertyTaxes: 0,
-        purchaseClosingCost: 0,
-        afterRepairValue: 0,
+      this.state = this.initialState();
+  }
 
-        downPaymentPercent: 0,
-        loanInterestRate: 0,
-        amortizedOverHowManyYears: 0,
+  initialState = () => {
+    return {
+      // Property Info
+      reportTitle: '',
+      propertyAddress: '',
+      city: '',
+      state: '',
+      zip: '',
+      
+      // Purchase Info
+      purchasePrice: 0,
+      repairCost: 0,
+      annualPropertyTaxes: 0,
+      purchaseClosingCost: 0,
+      afterRepairValue: 0,
 
-        // Rental Income
-        totalGrossMonthlyIncome: 0,
-        otherMonthlyIncome: 0,
-        electricity: 0,
-        waterAndSewer: 0,
-        garbage: 0,
-        pmi: 0,
-        hoas: 0,
-        monthlyInsurance: 0,
-        propertyTaxes: 0,
-        otherExpenses: 0,
+      downPaymentPercent: 0,
+      loanInterestRate: 0,
+      amortizedOverHowManyYears: 0,
 
-        vacancy: 0,
-        maintenance : 0,
-        capitalExpenditure: 0,
-        managementFee: 0,
+      // Rental Income
+      totalGrossMonthlyIncome: 0,
+      otherMonthlyIncome: 0,
+      electricity: 0,
+      waterAndSewer: 0,
+      garbage: 0,
+      pmi: 0,
+      hoas: 0,
+      monthlyInsurance: 0,
+      propertyTaxes: 0,
+      otherExpenses: 0,
 
-        annualIncomeGrowth: 0,
-        annualPVGrowth: 0,
-        annualExpensesGrowth: 0,
-        salesExpenses: 0,
-    };
+      vacancy: 0,
+      maintenance : 0,
+      capitalExpenditure: 0,
+      managementFee: 0,
+
+      annualIncomeGrowth: 0,
+      annualPVGrowth: 0,
+      annualExpensesGrowth: 0,
+      salesExpenses: 0,
+    }
   }
 
   changeHandler = event => {
@@ -73,7 +79,14 @@ class App extends Component {
 
   submitFormHandler = event => {
     event.preventDefault();
-    console.dir(this.refs.name.value); //will give us the name value
+    // Setting up properties "table" and push adds new
+    // object instead up resetting a single object
+    const propertiesRef = firebase.database().ref('properties');
+    propertiesRef.push({...this.state});
+    
+    // const resetstate = this.initialState();
+    // this.setState(resetstate)
+    return;
   }
 
   render() {
@@ -94,9 +107,9 @@ class App extends Component {
                 formDetails={formDetails.pentalInfo} 
                 onChange={this.changeHandler}
                 />
-                {/* <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit">
                   Submit
-                </Button> */}
+                </Button>
                 <br/>
                 <Analysis formState={this.state}/>
               </Form>
