61ba95bca1e9aa0f11aa6e8e99dea5a4a0f17572
Juan Duran <jcduran05@gmail.com>
2019-06-23 19:24:54 -0700

    Line graph created.

---
+++ src/Components/Analysis/LineGraph.js
@@ -0,0 +1,61 @@
+import React, { PureComponent } from 'react'
+import Chart from "chart.js";
+
+export default class LineGraph extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            schedule: props.schedule,
+            yearsAnalysis: props.years
+        }
+    }
+    lineChartRef = React.createRef();
+    
+    componentDidMount() {
+        const myLineRef = this.lineChartRef.current.getContext("2d");
+        let labels = this.state.years;
+        let balance = [];
+        let period = 13
+        for (let year in this.state.yearsAnalysis) {
+					if (this.state.yearsAnalysis[year] == 1) {
+						let b = this.state.schedule[period][3]
+						balance.push(Math.floor(b))
+					} else {
+						period = (12 * this.state.yearsAnalysis[year])
+						let b = this.state.schedule[period][3]
+						balance.push(Math.floor(b))
+					}
+				}
+
+        new Chart(myLineRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: ["Year 1","Year 2","Year 3","Year 5","Year 10","Year 15","Year 20","Year 30",],
+                datasets: [
+                    {
+												label: "Remaining Balanace",
+                        data: balance,
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+                legend: {
+                    display: false,
+                    position: 'right'
+                }
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+                <canvas
+                    id="myChart"
+                    ref={this.lineChartRef}
+                />
+            </div>
+        )
+    }
+}
--- src/Components/Analysis/OverYears.js
+++ src/Components/Analysis/OverYears.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Col, Row, Table, Button } from 'react-bootstrap';
+import LineGraph from './LineGraph';
 
 function OverYears(props) {
 		const p = props.data;		
-		const years = [1,2,3,4,5,10,15,20,30];
+		const years = [1,2,3,5,10,15,20,30];
 
-		console.log(p);
 		const yearsHead = years.map((th, idx) => {
 			return (
 				<th key={th}>Year {th}</th>
@@ -21,7 +21,40 @@ function OverYears(props) {
 				<td key={td}>{currentValue}</td>
 			)
 		});
-  
+
+		const pmt = (rate,nper,pv) => {
+			let pvif, pmt;
+	
+			pvif = Math.pow( 1 + rate, nper);
+			pmt = rate / (pvif - 1) * -(pv * pvif);   
+	
+			return pmt;
+	}
+
+	const computeSchedule = (loan_amount, interest_rate, payments_per_year, years, payment) => {
+			let schedule = [];
+			let remaining = loan_amount;
+			let number_of_payments = payments_per_year * years;
+	
+			for (let i=0; i<=number_of_payments; i++) {
+				let interest = remaining * (interest_rate/100/payments_per_year);
+				let principle = (payment-interest);
+				let row = [i, principle>0?(principle<payment?principle:payment):0, interest>0?interest:0, remaining>0?remaining:0];
+				schedule.push(row);
+				remaining -= principle
+			}
+	
+			return schedule;
+	}
+
+	let l_amount = 229000;
+	let l_ir = 3.9;
+	let l_ppy = 12;
+	let l_y = 30;
+	let l_p = 1080.12;
+	// console.log(computeSchedule(l_amount, l_ir, l_ppy, l_y, l_p))
+	let testSchedule = computeSchedule(l_amount, l_ir, l_ppy, l_y, l_p);
+
     return (
 			<Row className="row-border">
 				<Col md={12}>
@@ -40,6 +73,8 @@ function OverYears(props) {
 							
 					</tbody>
 				</Table>
+				<br/>
+				<LineGraph data={p} schedule={testSchedule} years={years} />
 				</Col>
 			</Row>
     );
